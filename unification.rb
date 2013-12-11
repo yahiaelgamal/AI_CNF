f1a0a32929a943205ac4632b41e0b4c52205e99b
DevYah <yahiaelgamal@gmail.com>
2013-12-11 19:12:53 +0200

    Probably a working pretty print

--- unification.rb
+++ unification.rb
@@ -1,3 +1,30 @@
+class Operator
+  # sym belongs to [^ v => <=>]
+  def initialize(sym)
+    self.sym = sym
+  end
+
+  def to_s
+    return self.sym
+  end
+end
+
+class Quant
+  # kind is either an A or an E
+  def initialize(kind)
+    self.kind = sym
+  end
+
+  def to_s
+    case self.kind
+    when 'A'
+      return "\u2200"
+    when 'E'
+      return "\u2203"
+    end
+  end
+end
+
 class Expression
   include Comparable
 
@@ -28,8 +55,65 @@ class Expression
   end
 end
 
-class Term < Expression
+class Sentence < Expression
+  
+  attr_accessor :type, :vars
+  # type is either ["atomic", "equiv", "neg", "op", "quant"]
+  # hash includes keys such as 
+  #     predicate (in case of atomic),
+  #     term1, term2 (in case of equiv)
+  #     sentence (in case of neg)
+  #     sentence1, sentence2, op (in case of "op")
+  #     quant, variable, sentence (in case of quan)
+  #     
+  def initialize(type, vars={})
+    @type = type
+    @vars= vars
+    unless vars.is_a?(Hash)
+      throw "Vars must be a Hash, it is a #{vars.class.name}"
+    end
 
+    case(type)
+    when "atomic"
+      unless vars.include?(:predicate)
+        throw "Vars must include :predicate, it has #{@vars.keys}"
+      end
+    when "equiv"
+      unless vars.include?(:term1) && vars.include?(:term2)
+        throw "Vars must include :term1, :term2, it has #{@vars.keys}"
+      end
+    when "neg"
+      unless vars.include?(:sentence)
+        throw "Vars must include :sentence, it has #{@vars.keys}"
+      end
+    when "op"
+      unless vars.include?(:op) && vars.include?(:sentence1) && vars.include?(:sentence2)
+        throw "Vars must include :sentence1, :sentence2, :op , it has #{@vars.keys}"
+      end
+    when "quant"
+      unless vars.include?(:quant) && vars.include?(:variable) && vars.include?(:sentence)
+        throw "Vars must include :quant, :variable, :sentence , it has #{@vars.keys}"
+      end
+    end
+  end
+
+  def to_s
+    case(@type)
+    when "atomic"
+      return @vars[:predicate].to_s
+    when "equiv"
+      return "#{@vars[:term1].to_s} = #{@vars[:term2].to_s}"
+    when "neg"
+      return "\u00AC(#{@vars[:sentence].to_s})"
+    when "op"
+      return "#{@vars[:sentence1].to_s} #{@vars[:op].to_s} #{@vars[:sentence2].to_s}"
+    when "quant"
+      return "#{@vars[:quant].to_s} #{@vars[:variable].to_s} #{@vars[:sentence].to_s}"
+    end
+  end
+end
+
+class Term < Expression
 end
 
 class Predicate < Term
@@ -110,9 +194,52 @@ class Variable < Term
   end
 end
 
+
+
+
+module CNF_Converter
+  def self.eliminate_equiv(sentence)
+    hash = sentence.hash
+    case sentence.type
+    when "equiv"
+      old_phi = hash[:sentence1]
+      old_shi = hash[:sentence2]
+
+      phi = eliminate_equiv(old_phi)
+      shi = eliminate_equiv(old_shi)
+
+      sentence1 = Sentence.new('op', {op: '=>', sentence1: phi, sentence2: shi})
+      sentence2 = Sentence.new('op', {op: '=>', sentence1: shi, sentence2: phi})
+
+      new_hash = {
+        op: '^',
+        sentence1: eliminate_equiv(hash[:sentence1]),
+        sentence2: eliminate_equiv(hash[:sentence2]),
+      }
+      return Sentence.new("op", new_hash)
+      eliminate_equiv(hash['term1'])
+    else
+      return sentence
+    end
+  end
+
+  def self.test_print
+    y = Variable.new('y')
+    f_y = Predicate.new('f', [y])
+    sentence = Sentence.new('neg', {sentence: Sentence.new('atomic', {predicate: f_y})})
+    puts sentence
+
+    g_y = Predicate.new('g', [y])
+    sentence2 = Sentence.new('op', {op: '<=>', sentence1: Sentence.new('atomic', {predicate: f_y}), sentence2: Sentence.new('atomic', {predicate: g_y})})
+    puts sentence2
+  end
+end
+
+
+
+
+
 class Unifier
-
-
   def unify e1, e2
     t = unify1 e1, e2, []
     puts "RESULT"
@@ -182,6 +309,7 @@ class Unifier
     puts '-'*40
 
     return ( unify1( ee1, ee2, unify1( t1, t2, u)))
+    zzc
   end
 
   def unify_var x, e, u
@@ -224,53 +352,56 @@ class Unifier
 end
 
 
-#example 1
-puts "="*50
-puts "Example 1"
-x = Variable.new 'x'
-a = Constant.new 'a'
-u = Variable.new 'u'
-v = Variable.new 'v'
-f1 = Predicate.new 'f', [a]
-f2 = Predicate.new 'f', [u]
-g1 = Predicate.new 'g', [x]
-g2 = Predicate.new 'g', [f1]
-p1 = Predicate.new 'P', [x, g1, g2]
-p2 = Predicate.new 'P', [f2, v, v]
-unifier = Unifier.new
-p p1
-p p2
-p unifier.unify p1, p2
+def test_unification
+  #example 1
+  puts "="*50
+  puts "Example 1"
+  x = Variable.new 'x'
+  a = Constant.new 'a'
+  u = Variable.new 'u'
+  v = Variable.new 'v'
+  f1 = Predicate.new 'f', [a]
+  f2 = Predicate.new 'f', [u]
+  g1 = Predicate.new 'g', [x]
+  g2 = Predicate.new 'g', [f1]
+  p1 = Predicate.new 'P', [x, g1, g2]
+  p2 = Predicate.new 'P', [f2, v, v]
+  unifier = Unifier.new
+  p p1
+  p p2
+  p unifier.unify p1, p2
 
 
-#example 2
-puts "="*50
-puts "Example 2"
-a = Constant.new 'a'
-y = Variable.new 'y'
-z = Variable.new 'z'
-u = Variable.new 'u'
-f = Predicate.new 'f', [y]
-p1 = Predicate.new 'P', [a, y, f]
-p2 = Predicate.new 'P', [z, z, u]
-unifier = Unifier.new
-p p1
-p p2
-p unifier.unify p1, p2
+  #example 2
+  puts "="*50
+  puts "Example 2"
+  a = Constant.new 'a'
+  y = Variable.new 'y'
+  z = Variable.new 'z'
+  u = Variable.new 'u'
+  f = Predicate.new 'f', [y]
+  p1 = Predicate.new 'P', [a, y, f]
+  p2 = Predicate.new 'P', [z, z, u]
+  unifier = Unifier.new
+  p p1
+  p p2
+  p unifier.unify p1, p2
 
-#example 3
-puts "="*50
-puts "Example 3"
-x = Variable.new 'x'
-z = Variable.new 'z'
-u = Variable.new 'u'
-g1 = Predicate.new 'g', [x]
-g2 = Predicate.new 'g', [z]
-g3 = Predicate.new 'g', [g2]
-g4 = Predicate.new 'g', [u]
-f1 = Predicate.new 'f', [x, g1, x]
-f2 = Predicate.new 'f', [g4, g3, z]
-unifier = Unifier.new
-p p1
-p p2
-p unifier.unify f1, f2
+  #example 3
+  puts "="*50
+  puts "Example 3"
+  x = Variable.new 'x'
+  z = Variable.new 'z'
+  u = Variable.new 'u'
+  g1 = Predicate.new 'g', [x]
+  g2 = Predicate.new 'g', [z]
+  g3 = Predicate.new 'g', [g2]
+  g4 = Predicate.new 'g', [u]
+  f1 = Predicate.new 'f', [x, g1, x]
+  f2 = Predicate.new 'f', [g4, g3, z]
+  unifier = Unifier.new
+  p p1
+  p p2
+  p unifier.unify f1, f2
+end
+#test_unification()
